21d2114013ef664fc95961d5b5411511072041a3
Aamir Raza <81976589+Merciful123@users.noreply.github.com>
2021-08-24 20:37:29 +0530

    Add files via upload
    Guessing Game : Here we're gonna have the computer to guess the number that you want.

---
+++ computer_guess.py
@@ -0,0 +1,19 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could be high also b/c low = high
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)??").lower
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+    print(f"yay, the computer guessed your {guess} correctly!")
+
+computer_guess(10)
